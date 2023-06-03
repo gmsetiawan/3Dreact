a0f87ab067cd3790f6f597e065c397bde5d5c2ca
Gunamullia Setiawan <gunamullia.setiawan@gmail.com>
2023-06-03 23:53:49 +0800

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import About from "./About";
+import Contact from "./Contact";
+import Experience from "./Experience";
+import Feedbacks from "./Feedbacks";
+import StarsCanvas from "./StarsCanvas";
+import Tech from "./Tech";
+import Works from "./Works";
+
+export {
+  Navbar,
+  Hero,
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  StarsCanvas,
+  Tech,
+  Works,
+};
